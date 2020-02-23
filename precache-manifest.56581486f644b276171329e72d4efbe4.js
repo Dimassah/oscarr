c6d98b7fff1c7c03c8498aa1c13b759306ff5fe1
Dimassah <adaqqeq@gmail.com>
2020-02-23 19:37:55 +0500

    Updates

---
+++ precache-manifest.56581486f644b276171329e72d4efbe4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f858839a5cdb78f6fab7774c3605794",
+    "url": "/Dimassah/oscarr.git/index.html"
+  },
+  {
+    "revision": "b1eea4061f25b78a152f",
+    "url": "/Dimassah/oscarr.git/static/css/main.9141de8e.chunk.css"
+  },
+  {
+    "revision": "006ba7ac8da4eb8bd068",
+    "url": "/Dimassah/oscarr.git/static/js/2.8b48db95.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Dimassah/oscarr.git/static/js/2.8b48db95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1eea4061f25b78a152f",
+    "url": "/Dimassah/oscarr.git/static/js/main.45c5b8d2.chunk.js"
+  },
+  {
+    "revision": "1063293a4a7bc48f4200",
+    "url": "/Dimassah/oscarr.git/static/js/runtime-main.53607b42.js"
+  },
+  {
+    "revision": "2be7c5bcd8d74e875667ac450f17ebc2",
+    "url": "/Dimassah/oscarr.git/static/media/home-1.2be7c5bc.jpg"
+  },
+  {
+    "revision": "dae70b7b7a92ee9d5408ecab637dc5f1",
+    "url": "/Dimassah/oscarr.git/static/media/home-2.dae70b7b.jpg"
+  },
+  {
+    "revision": "0392ae4e371a0b7bf8c6c84be9ccf654",
+    "url": "/Dimassah/oscarr.git/static/media/home-3.0392ae4e.jpg"
+  },
+  {
+    "revision": "8f982630dd7ba4488a99c30e3759bb6b",
+    "url": "/Dimassah/oscarr.git/static/media/oscarWiners-1.8f982630.jpg"
+  },
+  {
+    "revision": "7374738350c144b9e44d799b934937c8",
+    "url": "/Dimassah/oscarr.git/static/media/oscarWiners-2.73747383.jpg"
+  }
+]);
