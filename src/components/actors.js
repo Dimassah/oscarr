55b14bfede2cbf5be515cf2c3033c71d3014f4a0
Dimassah <adaqqeq@gmail.com>
2020-02-23 19:17:02 +0500

    test

---
+++ src/components/actors.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ActorsList } from '../data/actors';
+
+const Actors = (props) => {
+
+    let actors = ActorsList.map((person) => {
+        return (
+            <div className="actor-container">
+                <a>
+                    <div className="actor-image" style={{backgroundImage: "url(" + person.img_src + ")"}}></div>
+                </a>
+                    <h3>{person.name}</h3>
+            </div>
+        )
+    });
+    return (
+        <div className="main-content">
+            <div className="container">
+            {actors}
+            </div>   
+        </div>
+    );
+};
+
+export default Actors;
